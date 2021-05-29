77968d7b19f45ed81746f9db90e6b2e7fc1ffdc6
William <williamsantostr@gmail.com>
2021-05-29 14:31:50 -0300

    first steps

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/william/Área de Trabalho/Pasta Central/ROS Pastas/Studies_ROS/src
+# Build directory: /home/william/Área de Trabalho/Pasta Central/ROS Pastas/Studies_ROS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
